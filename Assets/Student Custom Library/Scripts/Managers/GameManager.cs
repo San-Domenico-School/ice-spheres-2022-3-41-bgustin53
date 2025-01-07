b5484e07b04b84bce95e233e46c2b5cb545af756
Bruce Gustin <35908618+bgustin53@users.noreply.github.com>
2025-01-07 15:11:35 -0800

    Task 2,2

--- Assets/Student Custom Library/Scripts/Managers/GameManager.cs
+++ Assets/Student Custom Library/Scripts/Managers/GameManager.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance;
+
+    // Player Fields
+    public Vector3 playerScale;
+    public float playerMass;
+    public float playerDrag;
+    public float playerMoveForce;
+    public float playerRepelForce;
+    public float playerBounce;
+
+    // Scene Fields
+    public GameObject[] waypoints;
+
+    // Debug Fields
+    public bool debugSpawnWaves;
+    public bool debugSpawnPortal;
+    public bool debugSpawnPowerUp;
+    public bool debugPowerUpRepel;
+
+    // Properties
+    public bool switchLevels { get; set; }
+    public bool gameOver { get; set; }
+    public bool playerHasPowerUp { get; set; }
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+
     }
+
+    private void Update()
+    {
+
+    }
+
+    private void EnablePlayer() { }
+    private void SwitchLevels() { }
 }
--- Assets/Student Custom Library/Scripts/Player Controls/PlayerController.cs
+++ Assets/Student Custom Library/Scripts/Player Controls/PlayerController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Fields
+    public Rigidbody playerRB;
+    public SphereCollider playerCollider;
+    public Light powerUpIndicator;
+    public PlayerInputActions inputAction;
+    public Transform focalpoint;
+    public float moveForceMagnitude;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Properties
+    public bool hasPowerUp { get; private set; }
+
+    // Methods
+    private void Awake() { }
+    private void Start() { }
+    private void OnEnable() { }
+    private void OnDisable() { }
+    private void FixedUpdate() { }
+    public void SetMoveDirection(Vector2 value) { }
+    public void AssignLevelValues() { }
+    private void Move() { }
+    private void OnCollisionEnter(Collision collision) { }
+    private void OnTriggerEnter(Collider other) { }
+    private void OnTriggerExit(Collider other) { }
+    private IEnumerator PowerUpCooldown(float cooldown) { }
 }
