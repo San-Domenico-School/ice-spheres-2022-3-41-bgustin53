821012da7e085ce2d1ee7fe7d64210fb4e6435a4
Bruce Gustin <35908618+bgustin53@users.noreply.github.com>
2025-01-07 19:36:06 -0800

    4.1.2 3.4

--- Assets/Student Custom Library/Scripts/Player Controls/PlayerController.cs
+++ Assets/Student Custom Library/Scripts/Player Controls/PlayerController.cs
@@ -5,26 +5,83 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Fields
-    public Rigidbody playerRB;
-    public SphereCollider playerCollider;
-    public Light powerUpIndicator;
-    public PlayerInputActions inputAction;
-    public Transform focalpoint;
-    public float moveForceMagnitude;
+    private Rigidbody playerRB;
+    private SphereCollider playerCollider;
+    private Light powerUpIndicator;
+    private PlayerInputActions inputAction;
+    private Transform focalpoint;
+    private float moveForceMagnitude;
+    private float moveDirection;
 
     // Properties
     public bool hasPowerUp { get; private set; }
 
     // Methods
-    private void Awake() { }
-    private void Start() { }
-    private void OnEnable() { }
-    private void OnDisable() { }
-    private void FixedUpdate() { }
-    public void SetMoveDirection(Vector2 value) { }
-    public void AssignLevelValues() { }
-    private void Move() { }
-    private void OnCollisionEnter(Collision collision) { }
+    private void Awake()
+    {
+        inputAction = new PlayerInputActions();
+    }
+        
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        playerRB = GetComponent<Rigidbody>();
+        playerCollider = GetComponent<SphereCollider>();
+        powerUpIndicator = GetComponent<Light>();
+        playerCollider.material.bounciness = 0.4f;
+        powerUpIndicator.intensity = 0;
+        
+    }
+
+    private void OnEnable()
+    {
+        inputAction.Enable();
+        inputAction.Player.Movement.performed += ctx => SetMoveDirection(ctx.ReadValue<Vector2>());
+        inputAction.Player.Movement.canceled += ctx => moveDirection = 0;
+    }
+
+    private void OnDisable()
+    {
+        inputAction.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void SetMoveDirection(Vector2 value)
+    {
+        Debug.Log(moveDirection);
+        moveDirection = value.y;
+    }
+
+    private void AssignLevelValues()
+    {
+        transform.localScale = GameManager.Instance.playerScale;
+        playerRB.mass = GameManager.Instance.playerMass;
+        playerRB.drag = GameManager.Instance.playerDrag;
+        moveForceMagnitude = GameManager.Instance.playerMoveForce;
+        focalpoint = GameObject.Find("Focal Point").transform;
+    }
+
+    private void Move()
+    {
+        if(focalpoint != null)
+        {
+            playerRB.AddForce(focalpoint.forward.normalized * moveDirection * moveForceMagnitude);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Startup"))
+        {
+            collision.gameObject.tag = "Ground";
+            playerCollider.material.bounciness = GameManager.Instance.playerBounce;
+            AssignLevelValues();
+        }
+    }
     private void OnTriggerEnter(Collider other) { }
     private void OnTriggerExit(Collider other) { }
     private IEnumerator PowerUpCooldown(float cooldown)
